8b32c24570b98f1b903c8bbc610feac16243a1d9
Kozintseva <100944116+Kozintseva@users.noreply.github.com>
2022-07-18 00:45:40 +0300

    Add files via upload

---
+++ FullTextSearch.sql
@@ -0,0 +1,41 @@
+USE Marketing
+GO
+
+--создаю каталог
+CREATE FULLTEXT CATALOG Marketing_FT_Catalog
+WITH ACCENT_SENSITIVITY = ON
+AS DEFAULT
+AUTHORIZATION [dbo]
+GO
+
+-- создаю индекс
+DROP FULLTEXT INDEX ON dbo.BulkEmail
+GO
+CREATE FULLTEXT INDEX ON dbo.BulkEmail(BodyLetter LANGUAGE Russian)
+KEY INDEX [PK__BulkEmai__0CFCA06C3C3356CA] -- первичный ключ
+ON (Marketing_FT_Catalog)
+WITH (
+  CHANGE_TRACKING = AUTO, 
+  STOPLIST = SYSTEM  
+);
+GO
+-- 3апускаем первоначальное заполнение
+ALTER FULLTEXT INDEX ON dbo.BulkEmail
+START FULL POPULATION  	
+
+
+--процедура поиска по колонке BulkEmail.BodyLetter
+
+CREATE OR ALTER PROCEDURE dbo.FindEmail
+@searchtext varchar(50)
+AS
+SELECT be.BulkEmailID as "ID рассылки", be.Name as "Наименование рассылки", be.StartSend as "Дата рассылки", be.BodyLetter as "Текст письма"
+	
+FROM dbo.BulkEmail be
+INNER JOIN FREETEXTTABLE(dbo.BulkEmail, BodyLetter,  @searchtext ) AS t
+ON be.BulkEmailID=t.[KEY]
+ORDER BY t.RANK DESC;
+GO
+
+--вызов процедуры
+exec dbo.FindEmail @searchtext='обновление системы'
